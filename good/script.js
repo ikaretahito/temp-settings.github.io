f6c44007f83415dc163b71a1c66c51b6113f3d82
ikaretahito <124506739+ikaretahito@users.noreply.github.com>
2025-09-17 17:02:14 +0900

    Update script.js
    storage usageもどき.png

--- good/script.js
+++ good/script.js
@@ -104,7 +104,7 @@ function bindStorage(){
       chart.src = "storage usageGB.png";
       chart.alt = "ストレージ円グラフ（GB）";
     }else{
-      chart.src = "storage usagePct.png";
+      chart.src = "storage usageもどき.png";
       chart.alt = "ストレージ円グラフ（%）";
     }
     segBtns.forEach(b => {
